565b002a7aa1ec3ebb238104b74f62e453a20a1a
kartemake <a.kuskov@make.st>
2023-12-11 12:22:08 +0700

    create variables

---
+++ variables/common.py
@@ -0,0 +1,3 @@
+timeout = 10
+modes = ['main', 'test']
+file_log = 'logs/log.txt'
---
+++ variables/time.py
@@ -0,0 +1,8 @@
+from func.func_datetime import getHourInMinutes
+
+time_start_work = getHourInMinutes(9)
+time_end_work = getHourInMinutes(18)
+time_start_dinner_break = getHourInMinutes(13)
+time_end_dinner_break = getHourInMinutes(14)
+
+time_live_cookie = 5

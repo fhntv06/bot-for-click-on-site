55a8682755dd1ee25ade6f779d3ffbb0a70c7f51
kartemake <a.kuskov@make.st>
2023-12-18 10:59:52 +0700

    correct message boot in driver

--- func/func_driver.py
+++ func/func_driver.py
@@ -1,23 +1,26 @@
 from selenium import webdriver
 from func.func_datetime import getAllDateAndTime
 from func.func_log import writeInLog
-from func.func_file import fileWork
+
+
+def boot():
+    writeInLog(f"{ '============================== BOOT ==============================' }\n")
+    writeInLog(f"Скрипт запущен в { getAllDateAndTime() }!\n")
 
 
 def initDriver():
-    fileWork()
-    # Создание экземпляра драйвера
+    boot()
+    writeInLog("Экземпляр драйвера Firefox создан!\n")
     return webdriver.Firefox()
 
 
 def quitDriver(driver):
-    separator = '============================== The End ==============================\n'
-
     driver.close()
 
-    writeInLog("\nЗакрыл браузер!")
+    writeInLog("Закрыл браузер!")
 
     driver.quit()
 
-    writeInLog(f'Завершил работу в { getAllDateAndTime() }\n\n{ separator }')
+    writeInLog(f'Завершил работу в { getAllDateAndTime() }\n')
+    writeInLog(f"{ '============================== The End ==============================' }\n")
 

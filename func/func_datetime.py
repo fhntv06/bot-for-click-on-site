63c91831b0ab877d213758a8ed6f55fb197ef0a8
kartemake <a.kuskov@make.st>
2024-01-12 10:48:57 +0700

    refactor getDateTimeNow

--- func/func_datetime.py
+++ func/func_datetime.py
@@ -3,29 +3,33 @@ import sys
 import time
 from datetime import datetime
 
-# Получаем текущее время
-current_datetime = datetime.now()
 
-values = {
-    'day': current_datetime.day,
-    'month': current_datetime.month,
-    'year': current_datetime.year,
-    'hour': current_datetime.hour,
-    'minute': current_datetime.minute,
-    'second': current_datetime.second,
-    'all': [
-        current_datetime.day,
-        current_datetime.month,
-        current_datetime.year,
-        current_datetime.hour,
-        current_datetime.minute,
-        current_datetime.second,
-    ]
-}
+def getDateTimeNow(value=''):
+    # Получаем текущее время
+    current_datetime = datetime.now()  # момент запуска кода
+
+    values = {
+        'day': current_datetime.day,
+        'month': current_datetime.month,
+        'year': current_datetime.year,
+        'hour': current_datetime.hour,
+        'minute': current_datetime.minute,
+        'second': current_datetime.second,
+        'all': [
+            current_datetime.day,
+            current_datetime.month,
+            current_datetime.year,
+            current_datetime.hour,
+            current_datetime.minute,
+            current_datetime.second,
+        ]
+    }
+
+    return values.get(value)
 
 
 def getHourInMinutes(hour='hour'):
-    forming_minutes = hour if isinstance(hour, (int, float)) else values.get('hour')
+    forming_minutes = hour if isinstance(hour, (int, float)) else getDateTimeNow('hour')
     return forming_minutes * 60
 
 
@@ -50,10 +54,10 @@ def formingCorrectStringDate(value):
 
 
 def getDatetimeCorrect(selector):
-    value = values.get(selector)
-
-    return formingCorrectStringDate(value)
+    return formingCorrectStringDate(
+        getDateTimeNow(selector)
+    )
 
 
 def getDatetime(selector):
-    return values.get(selector)
+    return getDateTimeNow(selector)
